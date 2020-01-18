60e45abaea7579e41f813dafe568112fccad9815
V Anand Shankar <vanandshankar@yahoo.com>
2020-01-17 21:17:34 -0800

    Modify data download script

--- src/data_download.py
+++ src/data_download.py
@@ -1,35 +1,20 @@
-"""Usage: data_download.py --SourceFileURL=<SourceFileURL> --TargetLocation=<TargetLocation>
+'''
+Download and save a csv file to a target location
 
-Process FILE and optionally apply correction to either left-hand side or
-right-hand side.
-Arguments:
-  SourceFileURL   URL to download file from
-  TargetLocation  Location where the file should be downloaded
-"""
+Usage: data_download.py --url=<url> --file_location=<file_location>
+ 
+'''
 
-import pandas as pd
-import numpy as np
+import requests
 from docopt import docopt
-import urllib.parse
 
 opt = docopt(__doc__)
 
-def main(source_file_url, target_location):
-  # Read csv for source url
-  data=pd.read_csv(source_file_url, index_col=0)
-  # Write csv to target location
-  data.to_csv(target_location)
-  
-# standard error function
-def sterror():
-    return 1
+def main(url, file_location):
+    # download data from url and save to file_location
+    r = requests.get(url)
+    with open(file_location, "wb") as f:
+        f.write(r.content) 
 
-def test_sterror():
-#   assert sterror(np.array([1, 1, 1])) == 0, "sterror should return 0 if vector values are all the same"
-    assert 1==1
-
-test_sterror()
-
-if __name__ == '__main__':
-    opt = docopt(__doc__)
-    main(opt['--SourceFileURL'], opt['--TargetLocation'])
+if __name__ == "__main__":
+    main(opt["--url"], opt["--file_location"])
