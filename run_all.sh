cfe5b7c841860b73a51b7d18ed746232ad1cece7
V Anand Shankar <vanandshankar@yahoo.com>
2020-01-24 17:44:31 -0800

    Update run_all.sh, comment out downloading for now

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 # python src/data_download.py --url="https://raw.githubusercontent.com/vanandsh/datasets/master/AB_NYC_2019.csv" --file_location="./data/raw_data.csv"
-# 
-# python src/wrangle.py --source_file_location="./data/raw_data.csv" --target_location="./data"
-# 
-# Rscript.exe src/eda_script.R --source_file="./data/train.csv" --target_location="./results"
-# 
+
+python src/wrangle.py --source_file_location="./data/raw_data.csv" --target_location="./data"
+
+Rscript.exe src/eda_script.R --source_file="./data/train.csv" --target_location="./results"
+
 python src/model.py --source_file_location="./data" --target_location="./results"
